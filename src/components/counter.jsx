e7ffcc25856ae10616398b4a7d59c781e4ae0aa0
Joseph <admin@josephncampbell.com>
2020-07-09 10:10:22 -0500

    updated changes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,16 +2,22 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    count: 13,
   };
 
   render() {
     return (
       <React.Fragment>
-        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
